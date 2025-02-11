1a3686110db70fe518ebf60cac5e8137d514b05f
Natalia <natacalv@emeal.nttdata.com>
2025-02-11 12:59:34 +0100

    Quinto commit: 09-useContext 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css'
+import { BrowserRouter } from "react-router-dom";
+
+
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
@@ -15,10 +18,13 @@ import './index.css'
 // import { CallbackHook } from "./06-memos/CallbackHook";
 // import { Padre } from "./07-tarea-memo/Padre";
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from "./08-useReducer/TodoApp";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-    <TodoApp/>
-  //</StrictMode>,
+  <BrowserRouter>
+    {/* <StrictMode> */}
+      <MainApp/>
+    {/* </StrictMode> */}
+  </BrowserRouter>
 )
